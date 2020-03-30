7e1f8e285c1544b86feeeda0c6a220b82f53ecfc
joseluisgs <joseluisgonsan@gmail.com>
2020-03-30 12:12:02 +0200

    Initial commit

---
+++ src/main/java/com/joseluisgs/productosapirestcliente/ProductosapirestClienteApplication.java
@@ -0,0 +1,13 @@
+package com.joseluisgs.productosapirestcliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductosapirestClienteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductosapirestClienteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joseluisgs/productosapirestcliente/ProductosapirestClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseluisgs.productosapirestcliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductosapirestClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
